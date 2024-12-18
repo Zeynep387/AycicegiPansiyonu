491c1ca93139f83e2803df10ee4a28ff055c7a58
Zeynep387 <221116034@alparslan.edu.tr>
2024-12-18 20:23:42 +0300

    Add files via upload
    Proje yüklendi

---
+++ AyçiçeğiÖdevi/Ayçiçeği Pansiyon Uygulaması v1.0/Ayçiçeği Pansiyon Uygulaması v1.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("Ayçiçeği Pansiyon Uygulaması v1.0")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ayçiçeği Pansiyon Uygulaması v1.0")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbd9fc82-d69e-406f-9104-70ceb3ff75e7")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
